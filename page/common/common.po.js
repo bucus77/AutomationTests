5cf4cb1871d062bdf80443faf7c1245b36ba3796
sebu <SebastiaBu@gmail.com>
2020-12-02 23:37:54 +0100

    finish fourh test

--- page/common/common.po.js
+++ page/common/common.po.js
@@ -8,21 +8,39 @@ const commonElements = require('./modules/commonElements.mo');
  * Used in checking main Page
  */
 class common {
-    async validHeaderLinks(expected){
+    async validHeaderLinks(expected) {
         const elements = commonElements.getHeaderLinks();
         const actualElements = await elements.getText();
-        await actualElements.splice(1,1);
+        await actualElements.splice(1, 1);
         const loginLink = await commonElements.getLoginLink().first();
         const actualLoginText = await loginLink.getText();
         await actualElements.push(actualLoginText);
-        const areElementsInlist = await helper.areElementsInList(expected,actualElements,'header link');
+        const areElementsInlist = await helper.areElementsInList(expected, actualElements, 'header link');
         return areElementsInlist;
     }
 
-    async validLeftNavbar(expected){
+    async validLeftNavbar(expected) {
         const elements = commonElements.getLeftNavbarLinks();
         const actualElements = await elements.getText();
-        const areElementsInlist = await helper.areElementsInList(expected,actualElements,'left nav bar');
+        const areElementsInlist = await helper.areElementsInList(expected, actualElements, 'left nav bar');
+        return areElementsInlist;
+    }
+
+    async validTopNav(expected) {
+        const elements = commonElements.getTopNav();
+        const actualElements = await elements.getText();
+        const areElementsInlist = await helper.areElementsInList(expected, actualElements, 'top nav');
+        return areElementsInlist;
+    }
+
+    async validLeftNavWraper(index, expected) {
+        const mainWrapper = await commonElements.getLeftNavWrapper().get(index);
+        await browser.actions().mouseMove(mainWrapper).perform();
+        const wrapper = commonElements.getWrapper(index);
+        const actualElements = await wrapper.getText();
+        console.log(actualElements[0]);
+        await actualElements.shift();
+        const areElementsInlist = await helper.areElementsInList(expected, actualElements, 'top nav');
         return areElementsInlist;
     }
 }
